1fa8e81513c8b9720b5fb8cc094097e4e8631f6e
kazuki-shimura <sim202025@icloud.com>
2020-11-18 18:02:48 +0900

    シリアライザーの作成

---
+++ api/serializers.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from .models import Profile, Review, Comment
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        # get_user_model()はDjango標準の関数でactiveなUserを取得する
+        model = get_user_model()
+
+        # serializerで扱いたいパラメーターの一覧を記述する
+        fields = ('id', 'email', 'password')
+
+        # パラメータの属性を指定する。（今回はパスワードは読み取られる事がないようにwrite_onlyにしている）
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+    # 登録した際にemailとpasswordが問題なく記載されれば
+    # validated_dataはバリデーションを通った後のデータとして引数として渡される
+    def create(self, validated_data):
+
+        # UserManagerクラスのメソッドを使用できるobjectsを使用してcreate_userを呼び出している
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
+
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    createdAt = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+    class Meta:
+        model = Profile
+        fields = ('id', 'nickName', 'userProfile', 'createdAt', 'avatar')
+        extra_kwargs = {'userProfile': {'read_only': True}}
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    createdAt = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+    class Meta:
+        model = Review
+        fields = ('id', 'title', 'bookName', 'content', 'userReview', 'createdAt', 'img', 'likedUser')
+        extra_kwargs = {'userReview': {'read_only': True}}
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'userComment', 'review')
+        extra_kwargs = {'userComment': {'read_only': True}}
+
+
