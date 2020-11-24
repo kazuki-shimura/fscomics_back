3e3566a0159c5e7c3c97f470d663886cc6328dad
kazuki-shimura <sim202025@icloud.com>
2020-11-24 11:18:19 +0900

    修正

--- fscomics_back/settings.py
+++ fscomics_back/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 # ○○○
-DEBUG = env('DEBGU')
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
